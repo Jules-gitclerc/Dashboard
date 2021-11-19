910e71ddcdfd4f7fcbfdef174e7680d9a4652bf2
Damien-EpiMaillard <damien.maillard@epitech.eu>
2021-11-19 11:54:02 +0100

    [~] refactor script start bdd

---
+++ dashboard-docker/start_bdd_dashboard.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE dashboard;
+
+USE dashboard;
+
+--Table User--
+
+CREATE TABLE User(
+    user_id int auto_increment,
+    username varchar(255) not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    phone varchar(255) not null,
+    is_identified bool,
+    primary key(user_id)
+);
+
+--Table Services--
+
+CREATE TABLE Services(
+    service_id int,
+    name varchar(255) not null
+);
+
+INSERT INTO Services(id_service, name) VALUES (1, 'Reddit');
+INSERT INTO Services(id_service, name) VALUES (2, 'League of legends');
